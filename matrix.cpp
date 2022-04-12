8ccf5d98e361ca9c384bb17add9680aa21aaa4ef
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-04-13 02:25:25 +0600

    ok

---
+++ matrix.cpp
@@ -0,0 +1,10 @@
+1/ Repeat step 2 to 4 for 1 = 1 to M 
+
+2/ Repeat step 3 to 4 for J = 1 to N
+
+3/ Set C[I,J] : = 0 
+
+4/ Repeat for K = 1 to P 
+    C[I,J] : = C[I,J] + A[I,K] * B[K,J]
+
+5/ Exit 
