e42a1aaf2e7e0daef4f1b82e1c835c0a6a951851
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-04-13 02:03:58 +0600

    ok

---
+++ insert_element_linear_array.cpp
@@ -0,0 +1,17 @@
+LA = Linear Array 
+N = number of elements
+K = Positive Integer 
+
+1/ Initialize [Set J = N]
+
+2/ Repeat steps 3 and 4 while J >= K 
+
+3/ set LA[J+1] = LA[J]
+
+4/ Set J = J - 1
+
+5/ set LA[K] = ITEM 
+
+6/ Set N = N + 1
+
+7/ Exit 
