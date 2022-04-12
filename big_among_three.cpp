7fc3a3ef9ae184931fff2b1d4201ff71bb80974f
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-04-13 00:44:19 +0600

    ok

---
+++ big_among_three.cpp
@@ -0,0 +1,11 @@
+1/ Start
+
+2/ input a,b,c 
+
+3/ if a is bigger than b and c, print a
+   if b is bigger than a and c, print b
+   if c is bigger than a and b, print c
+
+4/ print the LARGEST NUMBER 
+
+5/ End
