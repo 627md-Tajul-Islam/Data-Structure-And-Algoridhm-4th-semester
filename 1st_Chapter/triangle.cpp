4eca84e65e92122863a3884b61d18f0be92e7dc1
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-04-13 01:04:11 +0600

    ok

---
+++ 1st_Chapter/triangle.cpp
@@ -0,0 +1,10 @@
+1/ Start 
+
+2/ input a,b,c 
+
+3/ if a+b>c and b+c>a and c+a>b then 
+    s = (a+b+c)/2 
+    area = sqrt(s*(s-a)*(s-b)*(s-c))
+    print the area
+    else
+    triangle is not possible
